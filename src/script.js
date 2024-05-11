92ef2396395323c1a9b06214aaa11d464cd9559a
Ibrahimthedeveloper <iakande@dervac.com>
2024-05-11 15:35:20 +0100

    new comit

--- src/script.js
+++ src/script.js
@@ -55,4 +55,35 @@ function changeBackground() {
 }
 
 changeBackground(); // Start the slideshow
+  
+
+//arrow up section
+let mybutton = document.getElementById("go-up");
+//lets the user scrolls down 20px from the top of the document, and also show the button
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop >20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = 'block';
+    } else {
+        mybutton.style.display = 'none';
+    }
+}
+
+//When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; //for safari
+    document.documentElement.scrollTop = 0; // For chrome, Firefox, IE and Opera
+}
+
+
+
+
+
+
+
+
+
+
+
 
