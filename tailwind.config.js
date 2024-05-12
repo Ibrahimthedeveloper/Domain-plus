67c5834a94d3ffecf21e7a9eb674b64ad85fa051
DosuFE <sdosu@dervac.com>
2024-05-12 09:58:58 +0100

    my commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,15 @@ module.exports = {
       backgroundImage: {
         'pattern': "url('./images/slider-mainbg-01.jpg')",
         'headbg': "url('./images/pagetitle-bg.jpg')",
+        'shadow-4xl': '0 0 5px 0 rgba(0, 0, 0, 0.2)',
       }
     },
+
+    boxShadow: {
+      '4xl': '5px 5px 5px 5px rgba(0, 0, 0, 0.2)',
+      '3xl': '3px 3px 10px 5px #0b0b0b',
+      '2.5xl': '0 0 5px 3px rgba(0, 0, 0, 0.5)',
+    }
   },
   plugins: [],
 }
