012015ef280feaa6a604c27884d9180e4ea95f52
Webwiz <aliuanas004@gmail.com>
2024-07-03 12:39:16 +0100

    Update Script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -35,10 +35,6 @@ function openNav() {
 });
 
 
-
-
-
-
 const slideshow = document.getElementById('slideshow');
 const images = [
     './images/slider-mainbg-01.jpg',
@@ -473,6 +469,39 @@ const closeSupportModal = () => {
 }
 
 
+// readmore
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  // readmore
+function myFunction1() {
+    var dots1 = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
 
 
 
